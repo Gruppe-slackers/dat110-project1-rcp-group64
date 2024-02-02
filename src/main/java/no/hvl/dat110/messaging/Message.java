d3699d37db3d85ad9e2e0f781ec114442fcaf76c
669802 <111427312+669802@users.noreply.github.com>
2024-02-02 14:17:59 +0100

    Fixed constructor in Message.java

--- src/main/java/no/hvl/dat110/messaging/Message.java
+++ src/main/java/no/hvl/dat110/messaging/Message.java
@@ -5,11 +5,11 @@ import no.hvl.dat110.TODO;
 public class Message {
 
 	// the up to 127 bytes of data (payload) that a message can hold
-	private byte[] data;
+	private final byte[] data;
 
 	// construction a Message with the data provided
 	public Message(final byte[] data) {
-		if (data == null && data.length > 127) {
+		if (data == null || data.length > 127) {
 			throw new UnsupportedOperationException(TODO.constructor("Message"));
 		}
 		this.data = data;
