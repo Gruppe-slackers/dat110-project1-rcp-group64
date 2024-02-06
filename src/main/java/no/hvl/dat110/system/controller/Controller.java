4fa27313adac33148362369cb27a8e351d0caae5
GraffN <jorgen.graff-n@outlook.com>
2024-02-06 14:58:08 +0100

    create -> connect -> read controller

--- src/main/java/no/hvl/dat110/system/controller/Controller.java
+++ src/main/java/no/hvl/dat110/system/controller/Controller.java
@@ -26,17 +26,19 @@ public class Controller  {
 		// setup stop methods in the RPC middleware
 		RPCClientStopStub stopdisplay = new RPCClientStopStub(displayclient);
 		RPCClientStopStub stopsensor = new RPCClientStopStub(sensorclient);
-				
-		// TODO - START
-		
+
+
 		// create local display and sensor stub objects
+		display = new DisplayStub(displayclient);
+		sensor = new SensorStub(sensorclient);
+
 		// connect to sensor and display RPC servers - using the RPCClients
+		displayclient.connect();
+		sensorclient.connect();
+
 		// read value from sensor using RPC and write to display using RPC
-			
-		if (true)
-			throw new UnsupportedOperationException(TODO.method());
-		
-		// TODO - END
+		String value = String.valueOf(sensor.read());
+		display.write(value);
 
 		try {
 			stopdisplay.stop();
