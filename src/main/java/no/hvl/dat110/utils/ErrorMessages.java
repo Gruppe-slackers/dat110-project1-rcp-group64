2ee361c9566dc87b7d7fcbde06a3a4c337d4f270
gunni95 <gunni95@outlook.com>
2024-02-07 16:35:57 +0100

    RPCUtils now running

---
+++ src/main/java/no/hvl/dat110/utils/ErrorMessages.java
@@ -0,0 +1,15 @@
+package no.hvl.dat110.utils;
+
+public class ErrorMessages {
+    public static String lengthMissMatch() {
+        return "Message is not the same length as message has been assigned";
+    }
+
+    public static String maxLimit() {
+        return "Message received is too long";
+    }
+
+    public static String invalidType() {
+        return "Data received is invalid";
+    }
+}
